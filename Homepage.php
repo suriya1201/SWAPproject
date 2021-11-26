03b5fe988fc0bf565cb05b02a90b6130d2199727
suriya1201 <2001375A@student.tp.edu.sg>
2021-11-27 02:17:19 +0800

    header

--- Homepage.php
+++ Homepage.php
@@ -1,5 +1,47 @@
 <html>
+<style type="text/css">
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color:red;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: white;
+}
+/* Add a gray right border to all list items, except the last item (last-child) */
+li {
+  border-right: 2px solid #bbb;
+}
+
+li:last-child {
+  border-right: none;
+}
+</style>
 <head>
 
-<head>
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a class="active" href="#about">About</a></li>
+</ul>
+
+
+</head>
     </html>
