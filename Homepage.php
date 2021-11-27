982652f7c3e1e09bde0b3ea87867ff41f127ad11
suriya1201 <2001375A@student.tp.edu.sg>
2021-11-27 20:46:10 +0800

    ye

--- Homepage.php
+++ Homepage.php
@@ -14,11 +14,11 @@
             <nav>
                 <ul class="nav__links">
                     <li><a href="#">Services</a></li>
-                    <li><a href="#">Projects</a></li>
+                    <li><a href="#"></a></li>
                     <li><a href="#">About</a></li>
                 </ul>
             </nav>
-            <a class="cta" href="#">Contact</a>
+            <a class="cta" href="#">Login</a>
             
         </header>
         
