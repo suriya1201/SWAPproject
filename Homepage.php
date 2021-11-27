882df18da347af4430cdeae2b91e7f028bf56112
suriya1201 <2001375A@student.tp.edu.sg>
2021-11-27 15:21:05 +0800

    y

--- Homepage.php
+++ Homepage.php
@@ -43,7 +43,7 @@ footer {
 <head>
 
 <ul>
-  <li><a href="https://www.tp.edu.sg/home.html" ><img src="images/tplogo.png" alt="tp logo" width="100px" height="16px"></a></li>
+  <li><a href="https://www.tp.edu.sg/home.html" ><img src="images/" alt="tp logo" width="19px" height="16px"></a></li>
   <li><a href="#home">Home</a></li>
   <li><a href="#news">News</a></li>
   <li><a href="#contact">Contact</a></li>
