557e39a610f842c11c944fd4ec8093ae5fb539ac
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2021-11-28 23:14:17 +0800

    Update login.php

--- php functions/login.php
+++ php functions/login.php
@@ -5,7 +5,24 @@ include "db_connection.php";
 if(isset($_POST['username']) && isset($_POST['password']))  {
 
     function validate($data) {
-        
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return data;
     }
 }
+
+$username = validate($_POST['username']);
+$password = validate($_POST['password']);
+
+if(empty($username)) {
+    header("Location: Loginpage.php?erro=Username is required");
+    exit();
+}
+else if(empty($pass)) {
+    header("Location: Loginpage.php?erro=Username is required")
+    exit();
+}
+
+$sql = "SELECT"
 ?>
