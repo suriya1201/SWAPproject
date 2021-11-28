d2b352071cee2256c8e06d7cd56000fb70658b0d
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2021-11-28 18:30:07 +0800

    Update Loginpage.php

--- Loginpage.php
+++ Loginpage.php
@@ -1,29 +1,71 @@
 <!DOCTYPE html>
 <head>
+
+<style>
+body {  
+  display: block;
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;}
+form {
+  display: inline-block;
+  border: 3px solid #f1f1f1;
+  width: 600px;
+}
+
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.container {
+  padding-top: 50px;
+  padding-right: 30px;
+  padding-bottom: 50px;
+  padding-left: 30px;
+  width: 500px;
+}
+
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+</style>
 </head>
 <body>
 
-<h2>Login Form</h2>
-
-<form action="/action_page.php" method="post">
-
+<form action="/Loginpage.php" method="post">
 
   <div class="container">
-    <label for="uname"><b>Username</b></label>
+    <label for="username"><b>Username</b></label>
     <input type="text" placeholder="Enter Username" name="username" required>
 
-    <label for="psw"><b>Password</b></label>
+    <label for="password"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="password" required>
         
     <button type="submit">Login</button>
     <label>
       <input type="checkbox" checked="checked" name="remember"> Remember me
     </label>
-  </div>
 
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
+    <a href="#">Forgot password?</a>
   </div>
 </form>
 
