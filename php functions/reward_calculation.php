d59d60b44386fdd40325da7d3e12f9b8c6ed87ae
suriya1201 <2001375A@student.tp.edu.sg>
2021-11-29 01:06:23 +0800

    rewards

---
+++ php functions/reward_calculation.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+include "db_connection.php";
+
+if (isset($_SESSION["username"]))
+{
+	echo "<pre><b>Login done</b><br></pre>";
+	echo "<pre><h3>Welcome back <u>" . $_SESSION['username']. "</u></h3></pre>";
+	debug();
+}
+else {
+	echo "<pre><b>Login not done</b><br></pre>";
+	echo "<pre><h3><a href=loginform.php>You have not logged in. Please go back to login page</a></h3></pre>";
+	debug();
+	die("");
+}
+function debug() {
+	echo "<pre>";
+	echo "--------------------------------------------<br>";
+	echo "_SESSION<br>";
+	print_r($_SESSION);
+	echo "_COOKIE<br>";
+	print_r($_COOKIE);
+	echo "session_name()= " . session_name();
+	echo "<br>";
+	echo "session_id()= " . session_id();
+	echo "<br>";
+	echo "</pre>";
+}
+
+
+
+
+function calculatepoints($points,$username){
+    require_once "purchase.php";
+
+}
+
+
+
+?>
--- php functions/reward_logging.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-include "db_connection.php";
-
-if ()
-?>
