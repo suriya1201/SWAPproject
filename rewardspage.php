600fcb8896ac0f9d1c249b53c3224e364e7d052d
suriya1201 <2001375A@student.tp.edu.sg>
2021-11-28 19:39:18 +0800

    Update rewardspage.php

--- rewardspage.php
+++ rewardspage.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <a class="logo" href="https://www.tp.edu.sg/home.html"><img src="images/tplogo.png" alt="logo" width="60px" height="60px"></a>
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Rewards</a></li>
+                </ul>
+            </nav>
+            <a class="cta" href="#">USER IMAGE?</a>
+            
+        </header>
+        
+    
+    </body>
 
+
+    <footerss>
+    Copyright &copy; Temasek Polytechnic
+    </footer>
+</html>
