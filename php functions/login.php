628983f668b59078449569121c807110f8a9775a
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2021-11-28 23:36:01 +0800

    Update login.php

--- php functions/login.php
+++ php functions/login.php
@@ -16,13 +16,35 @@ $username = validate($_POST['username']);
 $password = validate($_POST['password']);
 
 if(empty($username)) {
-    header("Location: Loginpage.php?erro=Username is required");
+    header("Location: Loginpage.php?error=Username is required");
     exit();
 }
 else if(empty($pass)) {
-    header("Location: Loginpage.php?erro=Username is required")
+    header("Location: Loginpage.php?error=Username is required")
     exit();
 }
 
-$sql = "SELECT"
+$sql = "SELECT * FROM users WHERE user_name='$username' AND password='$password'";
+
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+    if($row['user_name'] === $username $$ $row['password'] === $passward) {
+        echo "Logged In";
+        $_SESSION['user_name'] = $row['user_name'];
+        $_SESSION['id'] = $row['id'];
+        header("Location: logged_in.php");
+        exit()
+    }
+    else{
+        header("Location: Loginpage.php?error=Incorrect Username or Password");
+        exit();
+    }
+}
+else {
+    header("Location: Loginpage.php");
+    exit();
+}
+
 ?>
