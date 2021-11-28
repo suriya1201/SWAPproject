53b5e1c1fe190b13a391b94bb8328d93333ca958
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2021-11-28 22:49:28 +0800

    Update login.php

--- php functions/login.php
+++ php functions/login.php
@@ -1,23 +1,6 @@
 <?php
-$con = mysqli_connect("localhost","root","","swap");
+session_start();
+include "db_connection.php";
 
-if (!$con){
-    die('Could not connect: ' . mysqli_connect_errno());
-}
-$query= $con->prepare("INSERT INTO `user` ( `USERNAME`, `PASSWORD`, `ADDRESS`, `PHONE_NUMBER` ) VALUES
-(?,?,?,?)");
-
-$name='user3';
-$pwd = 'pass';
-$address = 'ang mo kio ave 2';
-$contact = '11223344';
-
-$query->bind_param('ssss', $name, $pwd, $address,$contact);
-
-if ($query->execute()){
- echo "Query executed.";
-}
-else{
- echo "Error executing query.";
-}
+if()
 ?>
