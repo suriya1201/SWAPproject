215fb9c78f417c73e9620c699e755190486f12bd
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2021-12-19 03:14:09 +0800

    Update Homepage.php

--- webpage/Homepage.php
+++ webpage/Homepage.php
@@ -8,21 +8,7 @@
         <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="/w3css/3/w3.css">
     </head>
-    <body>
-        <header>
-            <a class="logo" href="https://www.tp.edu.sg/home.html"><img src="images/tplogo.png" alt="logo" width="60px" height="60px"></a>
-            <nav>
-                <ul class="nav__links">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="http://localhost/SWAPproject/webpage/rewardspage.php">Rewards</a></li>
-                </ul>
-            </nav>
-            <a class="cta" href="http://localhost/SWAPproject/webpage/Loginpage.php">Login</a>
-        </header>
-
-
-    <div class="row">
+    <?php include 'navbar.php' ?>
    
   
    <div class="column"><div padding=10px>item 1</div>
