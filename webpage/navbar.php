0377601b5f453f885e6b7a298fb8ec1c8f6b77e9
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2021-12-19 03:14:12 +0800

    Update navbar.php

--- webpage/navbar.php
+++ webpage/navbar.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-
-<head>
+    <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="description" content="">
@@ -8,17 +7,20 @@
         <link rel="stylesheet" href="styles.css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="/w3css/3/w3.css">
-</head>
-<body>
-<a class="logo" href="https://www.tp.edu.sg/home.html"><img src="images/tplogo.png" alt="logo" width="60px" height="60px"></a>
-        <nav>
-            <ul class="nav__links">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="http://localhost/SWAPproject/webpage/rewardspage.php">Rewards</a></li>
-            </ul>
-        </nav>
-    <a class="cta" href="http://localhost/SWAPproject/webpage/Loginpage.php">Login</a>
+    </head>
+    <body>
+        <header>
+            <a class="logo" href="https://www.tp.edu.sg/home.html"><img src="images/tplogo.png" alt="logo" width="60px" height="60px"></a>
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="http://localhost/SWAPproject/webpage/rewardspage.php">Rewards</a></li>
+                </ul>
+            </nav>
+            <a class="cta" href="http://localhost/SWAPproject/webpage/Loginpage.php">Login</a>
+        </header>
+    <div class="row">
+   
 </body>
-
 </html>
