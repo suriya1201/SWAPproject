c82d9e4f4a6cefed1fd35d247d4a28d81f99437a
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2021-12-20 02:35:38 +0800

    Update retriveprofile.php

--- webpage/retriveprofile.php
+++ webpage/retriveprofile.php
@@ -1,51 +1,27 @@
 <?php
 
-$sql="SELECT * FROM  Username = ?"
-$stmt->bind_param('s', $username);
-$stmt=$con->prepare($sql);
-$result = $stmt->execute();
-$result = $stmt->get_result();
+if (isset($_SESSION["Username"])){
 
-if(!$result) {
-    die("SELECT query failed<br>".$con->error);
-}else{
-    echo "SELECT QUERY SUCCESS<br>";
-}
+    $username = $_SESSION["Username"];
 
-$nrows=$result->num_rows;
-echo "number of rows=$nrows<br>";
 
-if($nrows>0){
-    echo "<table border=1>";
-    echo "<tr>";
-    echo "<th>Username</th>";
-    echo "<th>Email</th>";
-    echo "<th>Address</th>";
-    echo "<th>Phone_Number</th>";
+    $sql="SELECT * FROM  Username = '$username'"
+    $stmt->bind_param('s', $username);
+    $stmt=$con->prepare($sql);
+    $result = $stmt->execute();
+    $result = $stmt->get_result();
     
+    if($result->num_rows>0){
+        $username = $row['Username'];
+        $password = $row['Password'];
+        $email = $row['Email'];
+        $address = $row['Address'];
+        $contact = $row['Phone_number'];
+    }
 }
 
-While($row=$result->fetch_assoc()){
-    echo "<tr>";
-    echo "<td>";
-    echo $row['Username'];
-    echo "</td>";
-    echo "<td>";
-    echo $row['Email'];
-    echo "</td>";
-    echo "<td>";
-    echo $row['Address'];
-    echo "</td>";
-    echo "<td>";
-    echo $row['Phone_Number'];
-    echo "</td>";
-   
-    echo "</tr>";
-    
-}
-echo "</table>";
-else{
-    echo "0 records<br>";
-}
+
+
+
 
 ?>
