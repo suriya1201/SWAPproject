b1c4b9b8963785f49e133d1520318d13ed66a334
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2021-12-20 19:32:16 +0800

    Update retriveprofile.php

--- webpage/retriveprofile.php
+++ webpage/retriveprofile.php
@@ -1,27 +1,27 @@
 <?php
 
+include "db_connection.php";
+
 if (isset($_SESSION["Username"])){
 
     $username = $_SESSION["Username"];
 
-
-    $sql="SELECT * FROM  Username = '$username'"
-    $stmt->bind_param('s', $username);
-    $stmt=$con->prepare($sql);
+    $sql = "SELECT * FROM user WHERE Username = '$username'";
+    $stmt = $con->prepare($sql);
     $result = $stmt->execute();
     $result = $stmt->get_result();
     
     if($result->num_rows>0){
+        $row = mysqli_fetch_assoc($result);
         $username = $row['Username'];
-        $password = $row['Password'];
+        $password = base64_decode(hash("sha256", $row['Password']));
         $email = $row['Email'];
         $address = $row['Address'];
-        $contact = $row['Phone_number'];
+        $contact = $row['Phone_Number'];
+    }
+    else {
+        echo "error1";
     }
 }
-
-
-
-
 
 ?>
