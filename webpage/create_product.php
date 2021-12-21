6c3215da92712fc911791b9921e1a57c85e5be85
dariusstan <dariustan0433@gmail.com>
2021-12-21 21:51:31 +0800

    .

--- webpage/create_product.php
+++ webpage/create_product.php
@@ -1,25 +1,23 @@
-<html>
-<body>
 <?php
 $con = mysqli_connect("localhost","root","","tp_amc"); //connect to database
 if (!$con){
 die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail
 }
+
+$productname = $_POST['name'];
+$productdescription = $_POST['description'];
+$productprice = $_POST['price'];
+$productquantity = $_POST['quantity'];
+$productpoints = $_POST['points'];
+
+
 $query= $con->prepare("INSERT INTO `products` (`Product_Name`, `Product_Description`, `Price`, 
 `Quantity`, `Item_points`) VALUES
-(?,?,?,?,?)");
-$productname='product2';
-$productdescription = 'this is a test2';
-$productprice = '$100';
-$productquantity = '500';
-$productpoints = '200';
-$query->bind_param('sssss', $productname, $productdescription, $productprice, 
-$productquantity, $productpoints,); //bind the parameters
+('$productname', '$productdescription', '$productprice', '$productquantity', '$productpoints')");
+
 if ($query->execute()){ //execute query
  echo "Query executed.";
 }else{
  echo "Error executing query.";
 }
 ?>
-</body>
-</html>
---
+++ webpage/create_product_form.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+<form action="create_product.php" method="POST">
+    <input type="text" name="name" placeholder="productname">
+    <br>
+    <br>
+    <input type="text" name="description" placeholder="productdescription">
+    <br>
+    <br>
+    <input type="text" name="price" placeholder="productprice">
+    <br>
+    <br>
+    <input type="text" name="quantity" placeholder="productquantity">
+    <br>
+    <br>
+    <input type="text" name="points" placeholder="productpoints">
+    <br>
+    <br>
+    <button type="submit" name="submit">Add Product</button>
+</form>
+
+</body>
+</html>
