66271d161ffa8d6d605abb75740cde5bb52cb9b1
dariusstan <dariustan0433@gmail.com>
2021-12-22 17:35:36 +0800

    .

---
+++ webpage/display_product.php
@@ -0,0 +1,19 @@
+<?php
+$connect = mysqli_connect("localhost","root","","tp_amc");
+$query=$connect->prepare("SELECT ID, Product_Name, Product_Description, Price, Quantity, Item_points, Image FROM `products`");
+$query->execute();
+$query->bind_result($id, $productname, $productdescription, $productprice, $productquantity, $productpoints, $productimage );
+echo "<table align='center' border='1'><tr>";
+echo "<th>Id</th><th>Name</th><th>Description</th><th>Price</th><th>Quantity</th><th>Points</th><th>Image</th>";
+while($query->fetch())
+{
+    echo "<tr><td>".$id."</td>";
+    echo "<td>".$productname."</td>";
+    echo "<td>".$productdescription."</td>";
+    echo "<td>".$productprice."</td>";
+    echo "<td>".$productquantity."</td>";
+    echo "<td>".$productpoints."</td>";
+    echo "<td>".$productimage."</td>";
+}
+echo "</table>";
+?>
