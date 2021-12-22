b134e0f6133ecc8904369763a452b2b20a213ecf
olivier0724 <95130368+olivier0724@users.noreply.github.com>
2021-12-22 20:07:49 +0800

    Create create_purchase.php

---
+++ webpage/create_purchase.php
@@ -0,0 +1,15 @@
+<?php
+    $connect = mysqli_connect("localhost","root","","tp_amc");
+    $query = $connect->prepare("INSERT INTO `purchase` (`Shipping_information`,`Credit_card`,`Product_Name`,`Username`,`Custom_word`) VALUES (?,?,?,?,?)");
+    $Shipping_information = $_POST["Shipping_information"];
+    $Credit_card = $_POST["Credit_card"];
+    $Product_Name = $_POST["Product_Name"];
+    $Username = $_POST["Username"];
+    $Custom_word = $_POST["Custom_word"]
+    $query->bind_param('ss', $Shipping_information, $Credit_card, $Product_Name, $Username, $Custom_word);
+    if ($query->execute()){
+        echo "Query executed.";
+    }
+?>
+
+<meta http-equiv="refresh" content="0;URL=displayrewards.php" />
