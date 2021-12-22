554cab7f8648420b59f7be9e5a0ed0002e75b4c5
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2021-12-22 17:52:05 +0800

    Create footer.php

---
+++ webpage/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/w3css/3/w3.css">
+    </head>
+    <footer>
+    Copyright &copy; Temasek Polytechnic
+    </footer>
+</html>
