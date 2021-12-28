79edcf4929ebbdda0252d71c4364259218110f2d
suriya1201 <2001375A@student.tp.edu.sg>
2021-12-28 23:35:19 +0800

    Update member_recalculation.php

--- webpage/member_recalculation.php
+++ webpage/member_recalculation.php
@@ -1,13 +1,73 @@
 <?php 
-function recal() {
+
+function recal($Points,$User_id) {
+    require "config.php";
+    function printerror($message, $con) {
+		echo "<pre>";
+		echo "$message<br>";
+		if ($con) echo "FAILED: ". mysqli_error($con). "<br>";
+		echo "</pre>";
+	}
+
+	function printok($message) {
+		echo "<pre>";
+		echo "$message<br>";
+		echo "OK<br>";
+		echo "</pre>";
+	}
+
+	try {
+	$con=mysqli_connect($db_hostname,$db_username,$db_password);
+	}
+	catch (Exception $e) {
+		printerror($e->getMessage(),$con);
+	}
+	if (!$con) {
+		printerror("Connecting to $db_hostname", $con);
+		die();
+	}
+	else printok("Connecting to $db_hostname");
+
+	$result=mysqli_select_db($con, $db_database);
+	if (!$result) {
+		printerror("Selecting $db_database",$con);
+		die();
+	}
+	else printok("Selecting $db_database");
 
 
-    
+
+
+
+
+    $query="INSERT INTO tp_amc.rewards (Points,User_id) #insert values
+    VALUES ('$Points','$User_id')";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $result=mysqli_query($con,$query);
+	if (!$result) {
+		printerror("Selecting $db_database",$con);
+		die();
+	}
+	else printok($query);
+
+    mysqli_close($con);
+	printok("Closing connection");
+
+
+
 }
-
-
-
-
-
 
 ?>
