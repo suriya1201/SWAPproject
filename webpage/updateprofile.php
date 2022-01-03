269df38faa6e926b0c332436e915f12f4926565a
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2022-01-03 16:32:00 +0800

    Update updateprofile.php

--- webpage/updateprofile.php
+++ webpage/updateprofile.php
@@ -14,9 +14,10 @@ if (isset($_SESSION["Username"])) {
     $email  = validate($_POST['email']);
     $address = validate($_POST['address']);
     $contact = validate($_POST['phone-num']);
-    $sql = "UPDATE user SET Username='$username', Email='$email', Address='$address', Phone_number='$contact' WHERE Username = '$user'";
+    $sql = "UPDATE user SET Username=? , Email=? , Address=? , Phone_number=? WHERE Username = '$user'";
 
     if($stmt = $con->prepare($sql)) {
+        $stmt->bind_param('ssss', $username, $email, $address, $contact);
         $stmt->execute();
         echo "<script>alert('Profile Updated')</script>";
         header("Location: Profile_page.php");
