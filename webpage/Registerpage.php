b50fba372e2a0e2e93d8f7552bf9df6e08a0fd24
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2022-01-03 13:34:45 +0800

    Update Registerpage.php

--- webpage/Registerpage.php
+++ webpage/Registerpage.php
@@ -17,23 +17,35 @@
                 <p>Please fill in this form to create an account.</p>
                 <hr>
 
+                <div>
                 <label for="username"><b>Username</b></label>
                 <input type="text" placeholder="Enter Username" name="username" id="username" required>
+                </div>
 
+                <div>
                 <label for="psw"><b>Password</b></label>
                 <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+                </div>
 
+                <div>
                 <label for="psw-confirm"><b>Confirm Password</b></label>
                 <input type="password" placeholder="Confirm Password" name="psw-confirm" id="psw-confirm" required>
+                </div>
 
+                <div>
                 <label for="email"><b>Email</b></label>
                 <input type="email" placeholder="Enter Email" name="email" id="email" required>
+                </div>
 
+                <div>
                 <label for="address"><b>Address</b></label>
                 <input type="text" placeholder="Enter Address" name="address" id="address" required>
+                </div>
 
+                <div>
                 <label for="phone-num"><b>Phone Number</b></label>
                 <input type="text" placeholder="Enter Phone Number" name="phone-num" id="phone-num" required>
+                </div>
                 <hr>
                 <p>By creating an account you agree to our <a href="#">Terms & Policy</a>.</p>
 
