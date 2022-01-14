35db0f89c7f31b7716c8ac919d7c31a84ee1dad7
suriya1201 <2001375A@student.tp.edu.sg>
2022-01-14 20:26:54 +0800

    Update fxdelete_userrewards.php

---
+++ webpage/fxdelete_userrewards.php
@@ -0,0 +1,22 @@
+<?php 
+$connect = mysqli_connect("localhost","root","","tp_amc");
+
+
+$query= $connect->prepare("DELETE FROM rewards WHERE Points=0 ");
+if ($query->execute()) {
+    echo "Query executed.";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
