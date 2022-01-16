cd3906690e9986b981f2f72c7a0bff4d985f3f3f
suriya1201 <2001375A@student.tp.edu.sg>
2022-01-16 21:08:30 +0800

    Update usertype_rewards.php

--- webpage/usertype_rewards.php
+++ webpage/usertype_rewards.php
@@ -1,7 +1,7 @@
 <?php
 
 
-session_start();
+
 if (isset($_SESSION['username'])  && $_SESSION['User_type']=="member") {
    require_once "fxmember_calculation.php";
 
@@ -11,28 +11,19 @@ elseif(isset($_SESSION['username'])  && $_SESSION['User_type']=="VIP"){
    require_once "fxVIP_calculation.php";
 
 }
+elseif(isset($_SESSION['username'])  && $_SESSION['User_type']=="user"){
+	require_once "fxuser_calculation.php";
+}
 else{
-	require_once "fxuser_calculation.php";				
-	debug();
+				
+	
 	die("");
 }
 
 
 
 
-function debug() {
-	echo "<pre>";
-	echo "--------------------------------------------<br>";
-	echo "_SESSION<br>";
-	print_r($_SESSION);
-	echo "_COOKIE<br>";
-	print_r($_COOKIE);
-	echo "session_name()= " . session_name();
-	echo "<br>";
-	echo "session_id()= " . session_id();
-	echo "<br>";
-	echo "</pre>";
-}
+
 
 
 ?>
