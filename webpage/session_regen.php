be30d90b572d447ebbef2c146d34597331868b64
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2022-01-24 13:34:22 +0800

    session id regen part

--- webpage/session_regen.php
+++ webpage/session_regen.php
@@ -1,11 +1,12 @@
 <?php 
 session_start();
-
+session_regenerate_id();
 $inactive = 900;
 
 if (isset($_SESSION["timeout"])) {
     $sessionTTL = time() - $_SESSION["timeout"];
     if ($sessionTTL > $inactive) {
-    session_destroy();
-   
+        session_destroy();
+    }
+}
 ?>
