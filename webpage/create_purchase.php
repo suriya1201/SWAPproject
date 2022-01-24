151df5155f2e740e2a51f2907192953d54b43ae6
olivier0724 <95130368+olivier0724@users.noreply.github.com>
2022-01-24 13:05:16 +0800

    purchase

--- webpage/create_purchase.php
+++ webpage/create_purchase.php
@@ -1,13 +1,11 @@
 <?php
     $connect = mysqli_connect("localhost","root","","tp_amc");
-    $query = $connect->prepare("INSERT INTO `purchase` (`Shipping_information`,`Credit_card`,`Product_Name`,`Username`,`Quantity`,`Custom_word`) VALUES (?,?,?,?,?)");
-    $Shipping_information = $_POST["Shipping_information"];
-    $Credit_card = $_POST["Credit_card"];
+    $query = $connect->prepare("INSERT INTO `purchase` (`Product_Name`,`User_id`,`Quantity`,`Custom_word`) VALUES (?,?,?,?)");
     $Product_Name = $_POST["Product_Name"];
-    $Username = $_POST["Username"];
+    $User_id = $_POST["User_id"];
     $Quantity = $_POST["Quantity"];
     $Custom_word = $_POST["Custom_word"];
-    $query->bind_param('ssiiis', $Shipping_information, $Credit_card, $Product_Name, $Username, $Quantity, $Custom_word);
+    $query->bind_param('iiis',$Product_Name,$User_id,$Quantity, $Custom_word);
     if ($query->execute()){
         echo "Query executed.";
     }
