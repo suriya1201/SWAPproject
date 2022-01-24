e03044f4600fd11457184ffa03b6ed4617c8291e
jedrekk20 <95144832+jedrekk20@users.noreply.github.com>
2022-01-24 15:46:51 +0800

    fre

--- webpage/db_connection.php
+++ webpage/db_connection.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","tp_amc.sql");
+$con = mysqli_connect("localhost","root","","tp_amc");
 
 if (!$con){
     die('Could not connect: ' . mysqli_connect_errno());
