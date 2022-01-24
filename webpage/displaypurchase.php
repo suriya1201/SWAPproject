a3836a62c95ba465c07239050a3d175a73e8cd73
olivier0724 <95130368+olivier0724@users.noreply.github.com>
2022-01-24 14:02:01 +0800

    purchase stuff

--- webpage/displaypurchase.php
+++
@@ -1,7 +0,0 @@
-<form action="create_purchase.php" method="post">
-		Product Name: <input type="text" name="Product_Name"><br>
-		User ID: <input type="text" name="User_id"><br>
-		Quantity: <input type="text" name="Quantity"><br>
-		Custom Word: <input type="text" name="Custom_word"><br>
-		<input type="submit" value="Submit">
-	</form>
