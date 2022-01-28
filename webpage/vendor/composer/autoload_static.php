259da305d3e2170e7f36d2d93afb4820f8d8fbbf
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2022-01-28 15:23:24 +0800

    phpmailer

--- webpage/vendor/composer/autoload_static.php
+++ webpage/vendor/composer/autoload_static.php
@@ -11,6 +11,10 @@ class ComposerStaticInit08ff3a222d1f45f70a5265c420d81578
         array (
             'Sonata\\GoogleAuthenticator\\' => 27,
         ),
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
         'G' => 
         array (
             'Google\\Authenticator\\' => 21,
@@ -21,6 +25,10 @@ class ComposerStaticInit08ff3a222d1f45f70a5265c420d81578
         'Sonata\\GoogleAuthenticator\\' => 
         array (
             0 => __DIR__ . '/..' . '/sonata-project/google-authenticator/src',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'Google\\Authenticator\\' => 
         array (
--- webpage/vendor/composer/installed.php
+++ webpage/vendor/composer/installed.php
@@ -19,6 +19,15 @@
             'reference' => NULL,
             'dev_requirement' => false,
         ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.5.3',
+            'version' => '6.5.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'baeb7cde6b60b1286912690ab0693c7789a31e71',
+            'dev_requirement' => false,
+        ),
         'sonata-project/google-authenticator' => array(
             'pretty_version' => '2.3.1',
             'version' => '2.3.1.0',
