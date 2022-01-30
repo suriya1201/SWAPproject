dfcf70d98b1511a13b0a8c916f7f4cde8861c124
dariusstan <dariustan0433@gmail.com>
2022-01-30 18:16:20 +0800

    .

--- webpage/reset-password.inc.php
+++ webpage/reset-password.inc.php
@@ -4,8 +4,8 @@ if (isset($_POST["reset-password-submit"])){
 
     $selector = $_POST["selector"];
     $validator = $_POST["validator"];
-    $password = $_POST["pwd"];
-    $passwordRepeat = $_POST["pwd-repeat"];
+    $password = htmlspecialchars($_POST["pwd"]);
+    $passwordRepeat = htmlspecialchars($_POST["pwd-repeat"]);
 
     if(empty($password) || empty($passwordRepeat)){
 
