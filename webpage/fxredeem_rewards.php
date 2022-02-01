93b618b29ec9d5a29093d88a750b9a61b7aeaf0f
suriya1201 <2001375A@student.tp.edu.sg>
2022-02-01 23:33:49 +0800

    Update fxredeem_rewards.php

--- webpage/fxredeem_rewards.php
+++ webpage/fxredeem_rewards.php
@@ -4,26 +4,36 @@ include "db_connection.php";
 
 require "rewardspage.php";
 
-$reward = $reward_item;
-$reward_points = "SELECT reward_points FROM tp_amc.reward_types WHERE reward_item = $reward ";
+
+
+
+
+if (isset($_POST['submit'])){
+
 
 $User_id =$_SESSION['ID'];
+$reward = $reward_item;
+
+$query = $con->prepare("SELECT reward_points FROM tp_amc.reward_types WHERE reward_item = ? ");
+$query->bind_param('s', $reward);
+$query->execute();
+$result = $query->get_result();
+if ($result->num_rows > 0) {
+
+while($row = $result->fetch_assoc()) {
+    $reward_points = $row["reward_points"];
+}}
 
 
 
-$query=$con->prepare("UPDATE tp_amc.rewards SET Points = Points - $reward_points WHERE User_id=$User_id ");
-    
-  
 
-if ($query->execute()){
-        echo "Query executed, Database updated";
-    }
-    else{
-        echo $query->error;
-        echo "You do not have enough points";
-    }
+$query=$con->prepare("UPDATE tp_amc.rewards SET Points = Points - ? WHERE User_id= ? ");
+$query->bind_param('ii', $reward_points , $User_id);
+
+}
+
+
 require_once "fxdelete_userrewards.php";
 
 
-mysqli_close($con);
 ?>
