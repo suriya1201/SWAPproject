f672d661f2212c947812bc149e3e2b2cf3d71b17
suriya1201 <2001375A@student.tp.edu.sg>
2022-02-02 03:20:32 +0800

    about page

--- webpage/navbar.php
+++ webpage/navbar.php
@@ -14,7 +14,7 @@
             <nav>
                 <ul class="nav__links">
                     <li><a href="Homepage.php">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="about.php">About</a></li>
                     <?php
                     if (isset($_SESSION["Username"]) && isset($_SESSION["Role"]) && $_SESSION["Role"] == "p_admin") {
                         echo("<li><a href=create_product_form.php>Go back to adding page</a></li>");
