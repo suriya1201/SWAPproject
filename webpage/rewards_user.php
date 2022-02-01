7dbc6a2712db707abc3d4a45b0b246a050d9ff87
jedrekk20 <95144832+jedrekk20@users.noreply.github.com>
2022-02-02 00:00:46 +0800

    ds

---
+++ webpage/rewards_user.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
+        <style>
+	    .center {
+	        text-align: center;
+	    }
+	</style>
+    </head>
+    <body>
+        <?php include "navbar.php" ?>
+        <div class="row">
+
+        <?php
+        $connect = mysqli_connect("localhost","root","","tp_amc");
+        $query=$connect->prepare("SELECT ID, reward_item, reward_points FROM `reward_types`");
+        $query->execute();
+        $query->bind_result($id, $reward_item, $reward_points);
+        echo "<table align='center' border='1'><tr>";
+        echo "<th>Id</th><th>Rewards</th><th>Points</th>";
+        while($query->fetch())
+        {
+            echo "<tr><td>".$id."</td>";
+            echo "<td>".$reward_item."</td>";
+            echo "<td>".$reward_points."</td>";
+            echo "<td><a href='editrewards.php?id=".$id."'>edit</a></td>";
+            echo "<td><a href='deleterewards.php?id=".$id."'>delete</a></td></tr>";
+        }
+        echo "</table>";
+        ?>
+        <div class="column"><div padding=10px><a class="r1" href="http://localhost/SWAPproject/webpage/createrewards_form.php">Create Rewards</a></div>
+    </body>
+
+    <?php include 'footer.php' ?>
+
+</html>
