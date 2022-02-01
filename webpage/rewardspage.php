ce4b92ec28e62b0c0897ab5e51a830eb5a8f0d06
suriya1201 <2001375A@student.tp.edu.sg>
2022-02-02 00:04:55 +0800

    Update rewardspage.php

--- webpage/rewardspage.php
+++ webpage/rewardspage.php
@@ -1,11 +1,11 @@
 <?php 
 include "session_regen.php";
 if ($_SESSION['Role'] != 'r_admin') {
-    header("Location: rewards_admin.php");
+    header("Location: rewards_user.php");
 
 
 }elseif($_SESSION['Role'] == 'r_admin'){
-    header("Location: rewards_user.php");
+    header("Location: rewards_admin.php");
 
 
 }
