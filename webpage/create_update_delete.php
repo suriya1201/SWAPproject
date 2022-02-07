74e3993f22805864c5031cda425891862b5034e3
suriya1201 <2001375A@student.tp.edu.sg>
2022-02-07 23:11:18 +0800

    .

--- webpage/create_update_delete.php
+++ webpage/create_update_delete.php
@@ -1,11 +1,9 @@
 <?php
 include "db_connection.php";
 include "regex.php";
-$con = mysqli_connect("localhost","root","","tp_amc");
-if (!$con){
-	die('Could not connect: ' . mysqli_connect_errno());
-}
+include "session_regen.php";
 
+$user_id = $_SESSION['ID'];
 
 $Product_Name = validate($_POST["Product_Name"]);
 $Quantity = validate($_POST["Quantity"]);
@@ -42,5 +40,59 @@ if ($regex_check == 1) {
 
 
 
-$con->close();
+
+
+$name_product = $Product_Name;
+
+$query = $con->prepare("SELECT Item_points FROM tp_amc.products WHERE Product_Name = ?");
+$query->bind_param('s',$name_product );
+$query->execute();
+$result = $query->get_result();
+if ($result->num_rows > 0) {
+
+while($row = $result->fetch_assoc()) {
+    $item_points = $row["Item_points"];
+}}
+
+$Points = ($Quantity * $item_points ) ;
+
+
+
+
+
+
+$query=$con->prepare("SELECT EXISTS(SELECT User_id FROM tp_amc.rewards WHERE User_id = ?)");
+$query->bind_param('i',$user_id );
+$query->execute();
+$result = $query->get_result();
+    if ($result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+         $User_id = $row["User_id"];
+     }}
+     
+     if( !isset($User_id) || $User_id == ""){
+
+    $query=$con->prepare("INSERT INTO tp_amc.rewards (Points,User_id) VALUES (?,?)");
+    $query->bind_param('ii',$Points,$user_id );
+    $query->execute();
+
+
+
+
+     }
+     else{
+        $query=$con->prepare("UPDATE rewards SET Points = ? + Points WHERE User_id=? ");
+        $query->bind_param('ii',$Points,$user_id );
+        $query->execute();
+
+
+     }
+
+     $points = 0;
+     $query= $con->prepare("DELETE FROM rewards WHERE Points  = ?");
+     $query->bind_param('i', $points);
+     $query->execute();
+     
+
 ?>
--- webpage/regex.php
+++ webpage/regex.php
@@ -14,6 +14,6 @@ $username_regex = "/^[A-Za-z0-9 ]+$/"; #For password also
 $email_regex = "/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/";
 $contact_regex = "/\b\d{8}\b/";
 $address_regex = "/^[A-Za-z0-9 .,#-]+$/";
-$productname_regex = "/^[A-Za-z]+$/";
+$productname_regex = "/^[A-Za-z0-9]+$/";
 $quantity_regex = "/\b\d{3}\b/";
 ?>
