799942e48c31a1366f079519b39fda189afa7f05
dariusstan <dariustan0433@gmail.com>
2022-02-07 21:22:52 +0800

    ....

--- webpage/create_product.php
+++ webpage/create_product.php
@@ -13,7 +13,7 @@ if ($_SESSION['Role'] != 'p_admin') { //ensure only p_admin can access this page
 <?php
 $con = mysqli_connect("localhost","root","","tp_amc"); //connect to database
 if (!$con){
-die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail
+die('Could not connect: ' . mysqli_connect_errno()); //return error if connection fail
 }
 
 
