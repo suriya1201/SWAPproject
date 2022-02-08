8ddfd3e02808cbf5f54b92559212935a1f3de20c
lanzexi26 <63115311+lanzexi26@users.noreply.github.com>
2022-02-08 10:59:38 +0800

    Update updateprofile.php

--- webpage/updateprofile.php
+++ webpage/updateprofile.php
@@ -2,29 +2,29 @@
 include "db_connection.php";
 include "regex.php";
 
-if (!preg_match($username_regex, $username)){
-    echo "<script>alert('Please ensure that your username contains only numbers and alphabets')</script>";
-    $regex_check = 0;
-}
-if (!preg_match($email_regex, $email)){
-    echo "<script>alert('Please ensure that your email is correct')</script>";
-    $regex_check = 0;
-}
-if (!preg_match($address_regex, $address)){
-    echo "<script>alert('Please ensure that your address is correct')</script>";
-    $regex_check = 0;
-}
-if (!preg_match($contact_regex, $contact)){
-    echo "<script>alert('Only numbers within 8 digits is allowed')</script>";
-    $regex_check = 0;
-}
-if ($regex_check == 1) {
-    if (isset($_SESSION["Username"])) {
-        $user = $_SESSION["Username"];
-        $username = validate($_POST['username']);
-        $email  = validate($_POST['email']);
-        $address = validate($_POST['address']);
-        $contact = validate($_POST['phone-num']);
+if (isset($_SESSION["Username"])) {
+    $user = $_SESSION["Username"];
+    $username = validate($_POST['username']);
+    $email  = validate($_POST['email']);
+    $address = validate($_POST['address']);
+    $contact = validate($_POST['phone-num']);
+    if (!preg_match($username_regex, $username)){
+        echo "<script>alert('Please ensure that your username contains only numbers and alphabets')</script>";
+        $regex_check = 0;
+    }
+    if (!preg_match($email_regex, $email)){
+        echo "<script>alert('Please ensure that your email is correct')</script>";
+        $regex_check = 0;
+    }
+    if (!preg_match($address_regex, $address)){
+        echo "<script>alert('Please ensure that your address is correct')</script>";
+        $regex_check = 0;
+    }
+    if (!preg_match($contact_regex, $contact)){
+        echo "<script>alert('Only numbers within 8 digits is allowed')</script>";
+        $regex_check = 0;
+    }
+    if ($regex_check == 1) {
         $sql = "UPDATE user SET Username=?,Email=?,Address=?,Phone_number=? WHERE Username='$user'";
 
         if($stmt = $con->prepare($sql)) {
@@ -39,4 +39,5 @@ if ($regex_check == 1) {
         echo "Error";
     }
 }
+
 ?>
