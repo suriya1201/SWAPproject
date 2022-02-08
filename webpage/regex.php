f760293b58a04aa1a55fe635d7cb82fb5974774f
suriya1201 <2001375A@student.tp.edu.sg>
2022-02-08 11:22:53 +0800

    Update regex.php

--- webpage/regex.php
+++ webpage/regex.php
@@ -14,6 +14,7 @@ $username_regex = "/^[A-Za-z0-9 ]+$/"; #For password also
 $email_regex = "/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/";
 $contact_regex = "/\b\d{8}\b/";
 $address_regex = "/^[A-Za-z0-9 .,#-]+$/";
+
 $productname_regex = "/^[A-Za-z0-9]+$/";
 $quantity_regex = "/\b[0-9]{1,3}\b/";
 ?>
