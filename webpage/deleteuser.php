fb3b6025fed6967f9690c3eccbf6e0f59fd97d8f
olivier0724 <95130368+olivier0724@users.noreply.github.com>
2022-02-09 22:21:18 +0800

    Update deleteuser.php

--- webpage/deleteuser.php
+++ webpage/deleteuser.php
@@ -5,7 +5,7 @@ $id = $_GET['id'];
 $stmt = $con->prepare("DELETE FROM user WHERE ID=?");
 $stmt->bind_param('s', $id);
 if ($stmt->execute()) {
-    echo "<script>alert('user deleted')</script>";
-    header("view_users.php");
+    echo "<script>alert('User deleted!')</script>";
+    echo "<script>window.location.replace('view_users.php')</script>";
 }
 ?>
